cf8f84a38fda8698ff474b916b6600cb3ec31552
booking59 <ouzouighkamel@gmail.com>
2015-11-26 23:23:15 +0100

    added rendering ws service file

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,9 +1,17 @@
+import ratpack.groovy.template.TextTemplateModule
+import static ratpack.groovy.Groovy.groovyTemplate
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
+
+    bindings {
+        module TextTemplateModule, { TextTemplateModule.Config config -> config.staticallyCompile = true }
+    }
+
     handlers {
-        get {
-            render "Hello World"
+        post('getCountryFR') {
+            render groovyTemplate('services/country/getCountryByCodeResponse.xml')
         }
+
     }
 }
